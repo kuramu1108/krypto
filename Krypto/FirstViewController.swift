9e9502eac087e8cadbf98b83f5650b09b13c2bee
Po <colagarychen@gmail.com>
2020-05-29 10:41:48 +1000

    init xcode workspace and pod

---
+++ Krypto/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Krypto
+//
+//  Created by Po-Hao Chen on 2020/5/29.
+//  Copyright © 2020 Po-Hao Chen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Krypto/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Krypto
+//
+//  Created by Po-Hao Chen on 2020/5/29.
+//  Copyright © 2020 Po-Hao Chen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
