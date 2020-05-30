7c832ca146c3e4746b947eb79855bed071b60580
Po <colagarychen@gmail.com>
2020-05-30 18:40:14 +1000

    update api service

--- Krypto/api/ApiService.swift
+++ Krypto/api/ApiService.swift
@@ -13,12 +13,18 @@ class ApiService {
     
     static func getCurrentRate(base: Currency, quote: Currency) -> Observable<Rate> {
         let url = "https://rest.coinapi.io/v1/exchangerate/\(base.rawValue)/\(quote.rawValue)"
-        return request(url)
+        let path = Bundle.main.path(forResource: "keys", ofType: "plist")!
+        let keys = NSDictionary(contentsOfFile: path)!
+        let coinApiKey = keys["coinApiKey"] as? String
+        let headers: HTTPHeaders = [
+            "X-CoinAPI-Key": coinApiKey!
+        ]
+        return request(url, headers: headers)
     }
     
-    private static func request<T: Codable>(_ urlConvertible: String) -> Observable<T> {
+    private static func request<T: Codable>(_ urlConvertible: String, headers: HTTPHeaders? = nil) -> Observable<T> {
         return Observable<T>.create { observer in
-            let request = AF.request(urlConvertible).responseDecodable { (response: AFDataResponse<T>) in
+            let request = AF.request(urlConvertible, headers: headers).responseDecodable { (response: AFDataResponse<T>) in
                 switch response.result {
                 case .success(let value):
                     observer.onNext(value)
