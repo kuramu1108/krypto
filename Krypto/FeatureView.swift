85266574f0b0951a6fb7735f3b571dcada0c3e9f
yuhaoSong64 <66049658+yuhaoSong64@users.noreply.github.com>
2020-06-07 19:13:12 +1000

    homepage Update
    homepage Update

---
+++ Krypto/FeatureView.swift
@@ -0,0 +1,26 @@
+//
+//  FeatureView.swift
+//  Krypto
+//
+//  Created by 黄玥琪 on 6/6/20.
+//  Copyright © 2020 Po-Hao Chen. All rights reserved.
+//
+
+import UIKit
+
+class FeatureView: UIView {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var balanceLabel: UILabel!
+    
+    @IBOutlet weak var featureImageView: UIImageView!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
